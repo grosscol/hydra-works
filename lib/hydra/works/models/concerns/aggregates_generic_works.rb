d70da87b6942a9782a372815179703117eb91170
Colin Gross <grosscol@umich.edu>
2015-08-04 10:22:26 -0400

    Remove deprecated from aggregates_generic_works.

--- lib/hydra/works/models/concerns/aggregates_generic_works.rb
+++ lib/hydra/works/models/concerns/aggregates_generic_works.rb
@@ -5,16 +5,5 @@ module Hydra::Works
     def child_generic_work_ids
       child_generic_works.map(&:id)
     end
-
-    def generic_works= generic_works
-      warn "[DEPRECATION] `generic_works=` is deprecated.  Please use `child_generic_works=` instead.  This has a target date for removal of 07-31-2015"
-      self.child_generic_works = generic_works
-    end
-
-    def generic_works
-      warn "[DEPRECATION] `generic_works` is deprecated.  Please use `child_generic_works` instead.  This has a target date for removal of 07-31-2015"
-      self.child_generic_works
-    end
-
   end
 end
