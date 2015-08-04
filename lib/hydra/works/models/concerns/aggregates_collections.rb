b4b3104f5eb51d1146a892fe967d21ca4c186c80
Colin Gross <grosscol@umich.edu>
2015-08-04 11:11:23 -0400

    Remove aggregates_* from model concerns. All had only one convenience function for getting ids. The _ids functionality is implemented in activefedora-aggregation.

--- lib/hydra/works/models/concerns/aggregates_collections.rb
+++
@@ -1,9 +0,0 @@
-module Hydra::Works
-  # Allows instances of a class to aggregate (pcdm:hasMember) hydra-works collections
-  module AggregatesCollections
-
-    def child_collection_ids
-      child_collections.map(&:id)
-    end
-  end
-end
--- lib/hydra/works/models/concerns/aggregates_generic_works.rb
+++
@@ -1,9 +0,0 @@
-module Hydra::Works
-  # Allows instances of a class to aggregate (pcdm:hasMember) hydra-works generic works
-  module AggregatesGenericWorks
-
-    def child_generic_work_ids
-      child_generic_works.map(&:id)
-    end
-  end
-end
