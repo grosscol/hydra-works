d9aed8262eb40b1c9185b0db77bb9f90f1233b18
Colin Gross <grosscol@umich.edu>
2015-08-04 10:14:44 -0400

    Remove deprecated from aggregates_collections.

--- lib/hydra/works/models/concerns/aggregates_collections.rb
+++ lib/hydra/works/models/concerns/aggregates_collections.rb
@@ -5,16 +5,5 @@ module Hydra::Works
     def child_collection_ids
       child_collections.map(&:id)
     end
-
-    def collections= collections
-      warn "[DEPRECATION] `collections=` is deprecated.  Please use `child_collections=` instead.  This has a target date for removal of 07-31-2015"
-      self.child_collections = collections
-    end
-
-    def collections
-      warn "[DEPRECATION] `collections` is deprecated.  Please use `child_collections` instead.  This has a target date for removal of 07-31-2015"
-      self.child_collections
-    end
-
   end
 end
